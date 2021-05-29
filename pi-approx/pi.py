cbff1790d4f3a4807634c609fb29bae6060d800a
Naveen <naveensd101@gmail.com>
2021-05-29 21:37:47 +0530

    checked if uniform or random is better for this question  - preleminary result: uniform is better

--- pi-approx/pi.py
+++ pi-approx/pi.py
@@ -1,14 +1,40 @@
 import random
+import math
 
-inside_square = 0
-inside_circle = 0
+un = 0
+ra = 0
+def main():
+    print("HI")
+    inside_square = 0
+    inside_circle = 0
 
-for i in range(100001):
-    x = random.uniform(1,0)
-    y = random.uniform(1,0)
-    inside_square += 1
-    if( x * x + y * y < 1):
-        inside_circle += 1
+    _inside_square = 0
+    _inside_circle = 0
 
-pi = 4 * (inside_circle / inside_square)
-print("Aproximate pi value after "+ str(i) +" Iterations is = "+ str(pi))
+    un = 0
+    ra = 0
+
+    for i in range(100001):
+        x = random.uniform(1,0)
+        y = random.uniform(1,0)
+        _x = random.random()
+        _y = random.random()
+        inside_square += 1
+        _inside_square += 1
+        if( x * x + y * y < 1):
+            inside_circle += 1
+        if( _x * _x + _y * _y < 1):
+            _inside_circle += 1
+
+        pi = inside_circle / inside_square * 4
+        diff = abs(pi - math.pi)
+        un += diff
+        _pi = _inside_circle / _inside_square * 4
+        _diff = abs(_pi - math.pi)
+        ra += _diff
+
+        print(f"pi with unifor = {pi}\tdiff = {diff}\npi with random = {_pi}\tdiff = {_diff}\n\n")
+        print(f"uniform = {un}\nrandom = {ra}\n")
+
+if __name__ == "__main__":
+    main()
